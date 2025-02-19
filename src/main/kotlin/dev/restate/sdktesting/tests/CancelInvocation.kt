9601623e41d741f386d6f3974ee118a6c38b3aac
Francesco Guardiani <francescoguard@gmail.com>
2025-02-19 17:21:44 +0100

    Run CancelInvocation in always suspending configuration (#36)

--- src/main/kotlin/dev/restate/sdktesting/tests/CancelInvocation.kt
+++ src/main/kotlin/dev/restate/sdktesting/tests/CancelInvocation.kt
@@ -21,10 +21,12 @@ import kotlinx.coroutines.*
 import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.withAlias
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 
+@Tag("always-suspending")
 class CancelInvocation {
   companion object {
     @RegisterExtension
