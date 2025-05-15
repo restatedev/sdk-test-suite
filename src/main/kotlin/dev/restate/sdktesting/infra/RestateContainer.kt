9598b015f02582e97906bf8b4904d3f7c6a96abe
Till Rohrmann <till@restate.dev>
2025-05-15 14:32:47 +0200

    Set RESTATE_BIFROST__AUTO_RECOVERY_INTERVAL to 2s
    By setting the auto recovery interval to 2s, we can rely on the auto recovery
    to reconfigure the loglet in case of concurrent seal attempts by multiple CC
    leaders. This is a temporary solution until we have removed the LogsController
    from the CC.

--- src/main/kotlin/dev/restate/sdktesting/infra/RestateContainer.kt
+++ src/main/kotlin/dev/restate/sdktesting/infra/RestateContainer.kt
@@ -85,6 +85,7 @@ class RestateContainer(
                   "RESTATE_METADATA_SERVER__TYPE" to "replicated",
                   "RESTATE_METADATA_CLIENT__ADDRESSES" to
                       "[http://$RESTATE_RUNTIME:$RUNTIME_NODE_PORT]",
+                  "RESTATE_BIFROST__AUTO_RECOVERY_INTERVAL" to "2s",
               )
 
       return listOf(
