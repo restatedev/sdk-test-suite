e5857ee42948520c1f0bd630d33467d79f273486
Francesco Guardiani <francescoguard@gmail.com>
2025-12-17 09:56:58 +0100

    Bump restate sdk and testcontainers (#65)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,15 +44,16 @@ dependencyResolutionManagement {
       library("junit-reporting", "org.junit.platform", "junit-platform-reporting")
           .versionRef("junit-platform")
 
-      version("assertj", "3.27.3")
+      version("assertj", "3.27.6")
       library("assertj", "org.assertj", "assertj-core").versionRef("assertj")
 
-      version("testcontainers", "1.20.5")
+      version("testcontainers", "2.0.3")
       library("testcontainers-core", "org.testcontainers", "testcontainers")
           .versionRef("testcontainers")
-      library("testcontainers-kafka", "org.testcontainers", "kafka").versionRef("testcontainers")
+      library("testcontainers-kafka", "org.testcontainers", "testcontainers-kafka")
+          .versionRef("testcontainers")
       // Keep this in sync with the version used by testcontainers
-      library("docker", "com.github.docker-java:docker-java:3.4.1")
+      library("docker", "com.github.docker-java:docker-java:3.7.0")
 
       library("dotenv", "io.github.cdimascio:dotenv-kotlin:6.5.1")
 
